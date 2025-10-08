cf993511d1616f19ef638583db4274204468bc33
tej731 <160586019+tej731@users.noreply.github.com>
2025-10-08 23:36:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ def submit():
     return render_template('result.html', name=username, roll=rollno, mail=email, yr=year)
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5001)
+    app.run(host='0.0.0.0', port=5001, debug=True)
